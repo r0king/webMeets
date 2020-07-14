67ac35c3aaabcd8e15c38a24b2eb1276e7b7e2e3
rking12 <48954882+rking12@users.noreply.github.com>
2020-07-14 20:13:14 +0530

    few changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_login_screens/login_screen.dart';
 import 'package:flutter_login_screens/appcontent.dart';
 void main() {
   runApp(MyApp());
