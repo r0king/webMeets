2ba52596d96db95824103504b3cec78661e4da5f
rking12 <48954882+rking12@users.noreply.github.com>
2020-07-16 17:01:20 +0530

    curved nav

--- lib/webinarpage.dart
+++ lib/webinarpage.dart
@@ -1,65 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_login_screens/curved_navigation_bar.dart';
+
 class Thewebinarpage extends StatefulWidget {
   @override
   _ThewebinarpageState createState() => _ThewebinarpageState();
 }
 
 class _ThewebinarpageState extends State<Thewebinarpage> {
-
-  int _indexcurr=1;
+  int _indexcurr = 1;
   final _tabs = <Widget>[
     Center(
       child: Text("Contacts"),
     ),
     Container(
       child: Text("Meets"),
-    )
-  ,
-
+    ),
     Container(
       child: Text("Global meets"),
     )
   ];
 
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-
       child: Scaffold(
         backgroundColor: Colors.greenAccent,
-        body:_tabs[_indexcurr],
+        body: _tabs[_indexcurr],
+        bottomNavigationBar: CurvedNavigationBar(
 
+          height: 60,
+          backgroundColor: Colors.greenAccent,
+          items: <Widget>[
+            Icon(
+              Icons.contacts,size: 40,color: Colors.teal
+              ,
+            ),
+            Icon(
+              Icons.camera_enhance,size: 40,color: Colors.teal,
+            ),
+            Icon(
+              Icons.language,size: 40,color: Colors.teal,
 
-        bottomNavigationBar:
-        CurvedNavigationBar(
-
-            height: 60,
-            backgroundColor: Colors.greenAccent,
-            items: <Widget>[
-               Icon(
-                  Icons.contacts,),
-              Icon(
-                  Icons.camera_enhance,
-                ),
-              Icon(
-                    Icons.language,
-                  ),
-            ]
-        ,onTap: (index){
-              setState(() {
-                _indexcurr=index;
-              });
-        },
+            ),
+          ],
+          onTap: (index) {
+            setState(() {
+              _indexcurr = index;
+            });
+          },
           animationDuration: Duration(
             milliseconds: 200,
           ),
-
-
         ),
       ),
     );
   }
 }
-
